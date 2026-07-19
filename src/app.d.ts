216b97e23b9a4ec602bb1c0e7873c84bbdecafbe
RonY <sunnyrawat225@gmail.com>
2026-07-19 18:50:10 +0530

    chore: build fix

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,5 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
-/// <reference path="../worker-configuration.d.ts" />
 
 declare global {
 	namespace App {
@@ -8,12 +7,7 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		interface Platform {
-			env: Cloudflare.Env;
-			ctx: ExecutionContext;
-			caches: CacheStorage;
-			cf: IncomingRequestCfProperties;
-		}
+		// interface Platform {}
 	}
 }
 
